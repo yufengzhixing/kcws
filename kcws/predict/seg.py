93b887e3b254b6464725aceba47827d8c31c9656
御风之星 <diyanxing@163.com>
2017-07-07 10:05:30 +0800

    Update seg.py

--- kcws/predict/seg.py
+++ kcws/predict/seg.py
@@ -47,24 +47,24 @@ if __name__ == '__main__':
     x = graph.get_tensor_by_name('prefix/input_placeholder:0')
     t = graph.get_tensor_by_name('prefix/transitions:0')
     y = graph.get_tensor_by_name('prefix/Reshape_7:0')
-    z = graph.get_tensor_by_name('prefix/MatMul_1:0')
-    w = graph.get_tensor_by_name('prefix/MatMul_1:0')
-
 
     while True:
         input_string = raw_input('输入语句： > ')
         # 退出
         if input_string == 'quit':
             exit()
+        input_string_list = []
         input_string_vec = []
+        word_list = []
         num_string = len(input_string.decode("utf-8"))
         complement_list = [0]*(80-num_string)
 
         for word in input_string.decode("utf-8"):
             vec=int(vocab_dict.get(word.encode("utf-8"), -1))
+            input_string_list.append(word)
             input_string_vec.append(vec)
         input_vec=input_string_vec+complement_list
-        print input_vec
+        print input_string_vec
         with tf.Session(graph=graph) as sess:
 
             unary_score_val,transMatrix = sess.run([y,t], feed_dict={
@@ -72,8 +72,8 @@ if __name__ == '__main__':
             })
 
             feed_dict = {x: [input_vec]}
-            print 'transMatrix',transMatrix.shape,transMatrix
-            print 'score:',unary_score_val.shape,unary_score_val
+            # print 'transMatrix',transMatrix.shape,transMatrix
+            # print 'score:',unary_score_val.shape,unary_score_val
             for tf_unary_scores_, y_, sequence_length_ in zip(
                     unary_score_val, input_vec, [num_string]):
 
@@ -82,4 +82,16 @@ if __name__ == '__main__':
                     tf_unary_scores_, transMatrix)
 
             print "标签为：", viterbi_sequence
+        for i in range(num_string):
+            if viterbi_sequence[i] == 0:
+                print input_string_list[i]
+            elif (viterbi_sequence[i] > 0 and viterbi_sequence[i] <3):
+                word_list.append(input_string_list[i])
+            elif viterbi_sequence[i] == 3:
+                word_list.append(input_string_list[i])
+                print "".join(word_list)
+                word_list=[]
+
+
+
         print "*"*20,"end","*"*20
