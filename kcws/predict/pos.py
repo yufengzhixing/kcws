107a01a0cc675bd58a7872cb5f22fc529d0c7bb9
御风之星 <diyanxing@163.com>
2017-07-13 17:38:23 +0800

    Update pos.py

--- kcws/predict/pos.py
+++ kcws/predict/pos.py
@@ -30,6 +30,36 @@ def load_graph(frozen_graph_filename):
             producer_op_list=None
         )
     return graph
+def viterbi_decode(score, transition_params):
+  """Decode the highest scoring sequence of tags outside of TensorFlow.
+
+  This should only be used at test time.
+
+  Args:
+    score: A [seq_len, num_tags] matrix of unary potentials.
+    transition_params: A [num_tags, num_tags] matrix of binary potentials.
+
+  Returns:
+    viterbi: A [seq_len] list of integers containing the highest scoring tag
+        indicies.
+    viterbi_score: A float containing the score for the Viterbi sequence.
+  """
+  trellis = np.zeros_like(score)
+  backpointers = np.zeros_like(score, dtype=np.int32)
+  trellis[0] = score[0]
+
+  for t in range(1, score.shape[0]):
+    v = np.expand_dims(trellis[t - 1], 1) + transition_params
+    trellis[t] = score[t] + np.max(v, 0)
+    backpointers[t] = np.argmax(v, 0)
+
+  viterbi = [np.argmax(trellis[-1])]
+  for bp in reversed(backpointers[1:]):
+    viterbi.append(bp[viterbi[-1]])
+  viterbi.reverse()
+
+  viterbi_score = np.max(trellis[-1])
+  return viterbi, viterbi_score
 if __name__ == '__main__':
 
     char_dict=read_vocabulary('/home/di/pycharmProjects/segment/kcws-master/kcws/models/basic_vocab.txt')
@@ -84,7 +114,7 @@ if __name__ == '__main__':
         print unary_score_val.shape,transMatrix.shape
         for tf_unary_scores_, y_, sequence_length_ in zip(unary_score_val, input_string_vector, [num_word]):
             tf_unary_scores_ = tf_unary_scores_[:num_word]
-            viterbi_sequence, _ = tf.contrib.crf.viterbi_decode(tf_unary_scores_, transMatrix)
+            viterbi_sequence, _ = viterbi_decode(tf_unary_scores_, transMatrix)
 
         print "标签为：", viterbi_sequence
     for j in range(num_word):
