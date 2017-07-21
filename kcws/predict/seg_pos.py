903af8c7505d4f9276531a160b120a23eb77887d
御风之星 <diyanxing@163.com>
2017-07-21 10:23:59 +0800

    Update seg_pos.py

--- kcws/predict/seg_pos.py
+++ kcws/predict/seg_pos.py
@@ -64,138 +64,123 @@ def getPosWeights():
     y = graph.get_tensor_by_name('prefix/Reshape_9:0')
 
     return wx,cx,t,y,graph
-def viterbi_decode(score, transition_params):
-  """Decode the highest scoring sequence of tags outside of TensorFlow.
 
-  This should only be used at test time.
-
-  Args:
-    score: A [seq_len, num_tags] matrix of unary potentials.
-    transition_params: A [num_tags, num_tags] matrix of binary potentials.
-
-  Returns:
-    viterbi: A [seq_len] list of integers containing the highest scoring tag
-        indicies.
-    viterbi_score: A float containing the score for the Viterbi sequence.
-  """
-  trellis = np.zeros_like(score)
-  backpointers = np.zeros_like(score, dtype=np.int32)
-  trellis[0] = score[0]
-
-  for t in range(1, score.shape[0]):
-    v = np.expand_dims(trellis[t - 1], 1) + transition_params
-    trellis[t] = score[t] + np.max(v, 0)
-    backpointers[t] = np.argmax(v, 0)
-
-  viterbi = [np.argmax(trellis[-1])]
-  for bp in reversed(backpointers[1:]):
-    viterbi.append(bp[viterbi[-1]])
-  viterbi.reverse()
-
-  viterbi_score = np.max(trellis[-1])
-  return viterbi, viterbi_score
-
-if __name__ == '__main__':
+def segment(sentence):
     char_dict = read_vocabulary('/home/di/pycharmProjects/segment/kcws-master/kcws/models/basic_vocab.txt')
-    seg_x,seg_t, seg_y, seg_graph = getSegWeights()
+    seg_x, seg_t, seg_y, seg_graph = getSegWeights()
 
     word_dict = read_vocabulary('/home/di/pycharmProjects/segment/kcws-master/kcws/models/word_vocab.txt')
     pos_dict_temp = read_vocabulary('/home/di/pycharmProjects/segment/kcws-master/kcws/models/pos_vocab.txt')
-    pos_dict=dict(zip(pos_dict_temp.values(), pos_dict_temp.keys()))
-    pos_wx,pos_cx,pos_t,pos_y,pos_graph = getPosWeights()
+    pos_dict = dict(zip(pos_dict_temp.values(), pos_dict_temp.keys()))
+    pos_wx, pos_cx, pos_t, pos_y, pos_graph = getPosWeights()
+
+
+    input_string_list = []
+    input_string_vec = []
+    char_list = []
+    word_list = []
+    pos_list = []
+    num_string = len(sentence.decode("utf-8"))
+    seg_zero_list = [0] * (80 - num_string)
+
+    for word_m in sentence.decode("utf-8"):
+        vec = int(char_dict.get(word_m.encode("utf-8"), -1))
+        input_string_list.append(word_m)
+        input_string_vec.append(vec)
+    input_vec = input_string_vec + seg_zero_list
+    # print input_string_vec
+    with tf.Session(graph=seg_graph) as sess:
+        seg_feed_dict = {seg_x: [input_vec]}
+
+        seg_unary_score_val, seg_transMatrix = sess.run([seg_y, seg_t], seg_feed_dict)
+        # print 'transMatrix',transMatrix.shape,transMatrix
+        # print 'score:',unary_score_val.shape,unary_score_val
+        for seg_tf_unary_scores_, pos_y_, pos_sequence_length_ in zip(
+                seg_unary_score_val, input_vec, [num_string]):
+            seg_tf_unary_scores_ = seg_tf_unary_scores_[:num_string]
+            seg_viterbi_sequence, _ = tf.contrib.crf.viterbi_decode(
+                seg_tf_unary_scores_, seg_transMatrix)
+
+            # print "标签为：", seg_viterbi_sequence
+    for i in range(num_string):
+        if seg_viterbi_sequence[i] == 0:
+            word_s = input_string_list[i]
+            word_list.append(word_s)
+        elif (seg_viterbi_sequence[i] > 0 and seg_viterbi_sequence[i] < 3):
+            char_list.append(input_string_list[i])
+        elif seg_viterbi_sequence[i] == 3:
+            char_list.append(input_string_list[i])
+            word_m = "".join(char_list)
+            word_list.append(word_m)
+            char_list = []
+
+            # 词性标注部分
+
+    num_word = len(word_list)
+    input_string_vector = []
+    char_encode = []
+    char_vector = []
+    word_zero_list = [0] * (50 - num_word)
+
+    for w in word_list:
+        # print w
+        word_encode = int(word_dict.get(w.encode("utf8"), 0))
+        input_string_vector.append(word_encode)
+        num_char = len(w)
+        char_zero_list = [0] * (5 - num_char)
+
+        for c in w:
+
+            if num_char <= 5:
+                char_encode.append(int(char_dict.get(c.encode("utf8"), 0)))
+            else:
+                continue
+        if num_char < 5:
+            char_encode += char_zero_list
+        else:
+            continue
+        char_vector += char_encode
+        char_encode = []
+    char_vector = char_vector + [0] * (250 - len(char_vector))
+    input_string_vector += word_zero_list
+
+    # print input_string_vector,char_vector
+    with tf.Session(graph=pos_graph) as sess:
+        pos_feed_dict = {pos_wx: [input_string_vector],
+                         pos_cx: [char_vector]}
+        pos_unary_score, pos_transMatrix = sess.run([pos_y, pos_t], pos_feed_dict)
+
+        for pos_tf_unary_scores_, pos_y_, pos_sequence_length_ in zip(pos_unary_score, input_string_vector,
+                                                                      [num_word]):
+            pos_tf_unary_scores_ = pos_tf_unary_scores_[:num_word]
+            pos_viterbi_sequence, _ = tf.contrib.crf.viterbi_decode(pos_tf_unary_scores_, pos_transMatrix)
+
+            # print "词性标签为：", pos_viterbi_sequence
+    for j in range(num_word):
+        pos_word = pos_dict.get(str(int(pos_viterbi_sequence[j])), 0)
+        pos_list.append(pos_word)
+
+    return word_list,pos_list
+
+if __name__ == '__main__':
+
 
     while True:
         input_string = raw_input('输入语句： > ')
-        # 退出
+        sentence_seg,sentence_pos= segment(input_string)
+        seg_list=[]
+
+        for i in range (len(sentence_seg)):
+            newunit=sentence_seg[i]+"/"+sentence_pos[i]
+            seg_list.append(newunit)
+        print " ".join(seg_list)
+
+
+
         if input_string == 'quit':
             exit()
-        input_string_list = []
-        input_string_vec = []
-        char_list = []
-        word_list = []
-        num_string = len(input_string.decode("utf-8"))
-        seg_zero_list = [0] * (80 - num_string)
-
-        for word_m in input_string.decode("utf-8"):
-            vec=int(char_dict.get(word_m.encode("utf-8"), -1))
-            input_string_list.append(word_m)
-            input_string_vec.append(vec)
-        input_vec= input_string_vec + seg_zero_list
-        # print input_string_vec
-        with tf.Session(graph=seg_graph) as sess:
-            seg_feed_dict = {seg_x: [input_vec]}
-
-            seg_unary_score_val, seg_transMatrix = sess.run([seg_y, seg_t], seg_feed_dict)
-            # print 'transMatrix',transMatrix.shape,transMatrix
-            # print 'score:',unary_score_val.shape,unary_score_val
-            for seg_tf_unary_scores_, pos_y_, pos_sequence_length_ in zip(
-                    seg_unary_score_val, input_vec, [num_string]):
-
-                seg_tf_unary_scores_ = seg_tf_unary_scores_[:num_string]
-                seg_viterbi_sequence, _ = viterbi_decode(
-                    seg_tf_unary_scores_, seg_transMatrix)
-
-            # print "标签为：", seg_viterbi_sequence
-        for i in range(num_string):
-            if seg_viterbi_sequence[i] == 0:
-                word_s = input_string_list[i]
-                word_list.append(word_s)
-            elif (seg_viterbi_sequence[i] > 0 and seg_viterbi_sequence[i] <3):
-                char_list.append(input_string_list[i])
-            elif seg_viterbi_sequence[i] == 3:
-                char_list.append(input_string_list[i])
-                word_m = "".join(char_list)
-                word_list.append(word_m)
-                char_list = []
-
-#词性标注部分
-
-        num_word = len(word_list)
-        input_string_vector = []
-        char_encode = []
-        char_vector = []
-        word_zero_list = [0] * (50 - num_word)
-
-        for w in word_list:
-            print w
-            word_encode = int(word_dict.get(w.encode("utf8"), 0))
-            input_string_vector.append(word_encode)
-            num_char = len(w)
-            char_zero_list = [0] * (5 - num_char)
-
-            for c in w:
-
-                if num_char <= 5:
-                    char_encode.append(int(char_dict.get(c.encode("utf8"), 0)))
-                else:
-                    continue
-            if num_char < 5:
-                char_encode += char_zero_list
-            else:
-                continue
-            char_vector += char_encode
-            char_encode = []
-        char_vector = char_vector + [0] * (250 - len(char_vector))
-        input_string_vector += word_zero_list
-
-        print input_string_vector,char_vector
-        with tf.Session(graph=pos_graph) as sess:
-            pos_feed_dict = {pos_wx: [input_string_vector],
-                             pos_cx: [char_vector]}
-            pos_unary_score, pos_transMatrix = sess.run([pos_y, pos_t], pos_feed_dict)
-
-            for pos_tf_unary_scores_, pos_y_, pos_sequence_length_ in zip(pos_unary_score, input_string_vector, [num_word]):
-                pos_tf_unary_scores_ = pos_tf_unary_scores_[:num_word]
-                pos_viterbi_sequence, _ = tf.contrib.crf.viterbi_decode(pos_tf_unary_scores_, pos_transMatrix)
-
-            # print "词性标签为：", pos_viterbi_sequence
-        for j in range(num_word):
-            pos_word = pos_dict.get(str(int(pos_viterbi_sequence[j])), 0)
-            print word_list[j], pos_word
+    print "ok"
 
 
 
-        print "*"*20,"end","*"*20
-
-
-
+    print "*"*20,"end","*"*20
